dbd9d6aa86ba473570d3be06df841c7b6eb8049b
orcrobert <robyorban@gmail.com>
2025-03-14 14:37:06 +0200

    create nextjs proj

---
+++ src/components/navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <header className="h-16 flex items-center justify-between  top-0">
+            <ul className="flex gap-4">
+                <li><Link href="/" className="font-semibold">Home</Link></li>
+                <li><Link href="/" className="font-semibold">View</Link></li>
+                <li><Link href="/" className="font-semibold">Add band</Link></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Navbar;
