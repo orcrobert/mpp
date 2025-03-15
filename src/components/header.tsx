db967eb4d338d86a1491d9a2efb40343d4ea4c57
orcrobert <robyorban@gmail.com>
2025-03-15 13:27:58 +0200

    muie tabel heorui

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image";
+import { Button } from "@heroui/react"
+import Link from "next/link";
 
 const Header = () => {
     return (
         <>
-            <div>
+            <div className="w-full h-100 flex items-center justify-center overflow-hidden">
                 <Image
                     src="/lemmy.jpg"
                     alt="Description of the image"
                     width={1000}
                     height={1300}
+                    className="object-cover filter brightness-30"
                 />
+
+                <div className="absolute">
+                    <h2 className="text-white text-5xl font-black text-glow">Metal Bands</h2>
+                    <br />
+                    <div className="flex gap-5">
+                        <Button size="md" className="bg-gray-200 pt-2 pb-2 pr-3 pl-3 rounded-lg text-gray-900 w-30">View Bands</Button>
+                        <Link href="/addband"><Button size="md" className="bg-gray-200 pt-2 pb-2 pr-3 pl-3 rounded-lg text-gray-900 w-30">Add Band</Button></Link>
+                    </div>
+                </div>
             </div>
         </>
     )
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}", // App Router
+        "./pages/**/*.{js,ts,jsx,tsx}", // Pages Router
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/@heroui/react/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
