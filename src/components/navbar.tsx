8f79d841f5774fac7f865cc6444e27671073d14d
orcrobert <robyorban@gmail.com>
2025-03-17 18:47:30 +0200

    foarte productiv

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,14 @@
-import Link from "next/link";
+// import Link from "next/link";
+import { Link } from "@chakra-ui/react"
 import AddBandPage from "@/app/addband/page";
 
 const Navbar = () => {
     return (
         <header className="h-16 flex items-center justify-between  top-0">
             <ul className="flex gap-4">
-                <li><Link href="/" className="font-semibold">Home</Link></li>
-                <li><Link href="#table-view-section" className="font-semibold">View</Link></li>
-                <li><Link href="/addband" className="font-semibold">Add band</Link></li>
+                <li><Link href="/" fontSize="sm" fontWeight="bold">Home</Link></li>
+                <li><Link href="#table-view-section" fontSize="sm" fontWeight="bold">View</Link></li>
+                <li><Link href="/addband" fontSize="sm" fontWeight="bold">Add band</Link></li>
             </ul>
         </header>
     )
