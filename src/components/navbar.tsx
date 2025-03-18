fa9e58168057f2edac53f1e151e6007bf5d23ad8
orcrobert <robyorban@gmail.com>
2025-03-18 21:38:02 +0200

    s

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,8 @@ import AddBandPage from "@/app/add-band/page";
 const Navbar = () => {
     return (
         <header className="h-16 flex items-center justify-between top-0">
-            <ul className="flex gap-4">
+            <div className="dark:bg-green-500 blur-3xl h-10 w-50 absolute z-0 opacity-70"></div>
+            <ul className="flex gap-4 z-10">
                 <li><Link href="/" fontSize="sm" fontWeight="bold">Home</Link></li>
                 <li><Link href="#table-view-section" fontSize="sm" fontWeight="bold">View</Link></li>
                 <li><Link href="/add-band" fontSize="sm" fontWeight="bold">Add band</Link></li>
