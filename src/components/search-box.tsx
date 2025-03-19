d393bb20db0bd5b4d81f0f33fc098d920a2a79a1
orcrobert <robyorban@gmail.com>
2025-03-19 13:03:43 +0200

    more style

--- src/components/search-box.tsx
+++ src/components/search-box.tsx
@@ -2,15 +2,16 @@ import { Box, Input, Heading } from "@chakra-ui/react"
 
 export function SearchBox({ onSearch }: { onSearch: (query: string) => void }) {
     return (
-        <Box marginTop="12" marginBottom="6" backgroundColor="gray.950" p={6} borderRadius={12}>
+        <Box marginTop="12" marginBottom="6" backgroundColor="gray.950" p={6} borderRadius={6}>
             <Box display="flex" gap={3}>
-                <Heading fontWeight="bold" fontSize={25} marginBottom={3}>Search Database</Heading>
+                <Heading fontWeight="bold" fontSize={16} marginBottom={3}>Search Database</Heading>
                 <Box fontSize={25}>🔍</Box>
             </Box>
             <Input
                 placeholder="Search"
                 variant="outline"
                 onChange={(e) => onSearch(e.target.value)}
+                borderRadius={6}
             />
         </Box>
     );
